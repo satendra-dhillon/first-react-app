d39c372219551d76bb2c940a994d3592bf51a33f
satendra singh <satendra@meter.taxi>
2018-10-31 17:45:36 +0530

    added message component

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -3,19 +3,14 @@ import React from 'react';
 // import GameCard from "./GameCard";
 import GameCard from './GameCrad';
 import PropTypes from 'prop-types';
+import Message from './Message';
 
 
 const GamesList = ( {games} ) => (
     <div className= "ui four cards">
         {
             games.length === 0 ? (
-                <div className="ui icon message">
-                    <i className="icon info"></i>
-                    <div className="content">
-                        <div className="header"> There are no game in your store!</div>
-                        <p> you should add some, don't you think</p>
-                    </div>
-                </div>
+                <Message />
             ) : (
                 games.map(game => <GameCard game={game} key={game._id} />)
             )
@@ -28,9 +23,9 @@ GamesList.propTypes = {
     games: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
-GamesList.defaultProps = {
-    games = []
-}
+// GamesList.defaultProps = {
+//     games = []
+// }
 
 export default GamesList;
 
---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = () => (
+    <div className="ui icon message">
+        <i className="icon info"></i>
+        <div className="content">
+            <div className="header"> There are no game in your store!</div>
+            <p> you should add some, don't you think</p>
+        </div>
+    </div>
+);
+
+export default Message;
